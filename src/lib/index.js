e7b81dcac47606524d7d1f3d1994750618672105
Albert Mends <mendsalbert@gmail.com>
2024-05-10 06:49:54 +0000

    update solidity-files-cache.json, journal.jsonl, SupplyGuard.json and src/lib/index.js update hardhat.config.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,11 +4,10 @@ import SupplyGuard from "./SupplyGuard.json";
 export const contract = async () => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const { ethereum } = window;
-  // prevAddr = 0x05a9c0eb1CB700f6B86eF159FeCf0Cc82d8EA59D
   if (ethereum) {
     const signer = provider.getSigner();
     const contractReader = new ethers.Contract(
-      "0x03EC49cbA10b6005abE9E69ff36A7728fB72acD6",
+      "0x0C4361c038696dB4e2A28367689e60eE6F8F94FB",
       SupplyGuard.abi,
       signer
     );
